a64077ab8bdbde35c5d54231ee352135789b4a05
arafatGithubCode <md489288@gmail.com>
2024-11-20 22:08:15 +0600

    update new feature section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,26 +22,23 @@
     });
   }
 
-  // New Addition Slider
-  $(".feature-slider").owlCarousel({
-    items: 4,
+  // owl-banner (new addition section)
+  $(".owl-banner").owlCarousel({
+    center: true,
+    items: 1,
     loop: true,
-    dots: true,
-    nav: false,
-    autoplay: true,
-    margin: 5,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    margin: 30,
     responsive: {
-      0: {
+      992: {
         items: 1,
       },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-      },
-      1600: {
-        items: 4,
+      1200: {
+        items: 1,
       },
     },
   });
